71bd3ffa33512c30416d76ef99711f504de21522
killerxoffical <mdg145712@gmail.com>
2026-07-23 16:58:15 +0600

    Add files via upload

---
+++ profiles.js
@@ -0,0 +1,20 @@
+// ==========================================
+// PRE-SAVED GITHUB PROFILES
+// ==========================================
+// You can add as many profiles here as you want.
+// Just make sure to keep the format exactly the same.
+// WARNING: Anyone who can see your Netlify page source can see these tokens. 
+// Keep this link private for your admins only!
+
+const PREDEFINED_PROFILES = {
+    "BOOYAH BACKEND": {
+        username: "killerxoffical",
+        repo: "booyah-backend",
+        token: "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"
+    },
+    "Test Repo": {
+        username: "killerxoffical",
+        repo: "test-app",
+        token: "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"
+    }
+};
